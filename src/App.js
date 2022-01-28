ef35fba87c558747dd7fee739bea7929149b7f31
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-01-28 22:35:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+import React from "react";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+import AddTask from "./components/AddTask";
 function App() {
+  const[tasks,setTasks]=useState([
+    {
+        id:1,
+        text:'Doctors Appointment',
+        day:'Feb 5th at 2:10pm',
+        remainder: true,
+    },
+    {
+        id:2,
+        text:'Ennggs Appointment',
+        day:'May 5th at 2:10pm',
+        remainder: false,
+    },
+    {
+        id:3,
+        text:'CA Appointment',
+        day:'March 5th at 2:10pm',
+        remainder: true,
+    }
+
+])
+// add task
+const addTask=(task)=>{
+  const id=Math.floor(Math.random()*1000)+1
+  console.log(id)
+  const newTask={id,...task}
+  setTasks([...tasks,newTask])
+}
+// delete tasks
+const deleteTask=(id)=>{
+  setTasks(tasks.filter((task)=>task.id!==id))
+}
+const toggleRemainder=(id)=>{
+  setTasks(tasks.map((task)=>task.id===id?{...task,remainde:!task.remainder}:task))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Task Tracker" />
+      <AddTask onAdd={addTask} />
+      {tasks.length>0?<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRemainder}/>:'No Tasks '}
+      
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import Button from './Button';
+
+const Header = (props) => {
+    const onClick=()=>{
+        console.log("Clicked")
+    }
+  return <div>
+      <header className='header'>
+      <h1>{props.title}</h1>
+        <Button color='green' text='Add' onClick={onClick}/>
+      
+      </header>
+      
+  </div>;
+};
+
+export default Header;
